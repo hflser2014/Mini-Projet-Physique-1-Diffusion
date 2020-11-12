3feb9ab299191dc562995f6734c8095d9b1f8a20
AdrienZ <zhuch.edu@outlook.com>
2020-11-12 10:53:30 +0800

    3d版本独立建立了文件，修复了位移显示bug

--- brownMovement.py
+++ brownMovement.py
@@ -1,87 +1,33 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import random
-from mpl_toolkits.mplot3d import Axes3D
+from scipy import stats
 
-def brownMovement(N,t,n,starting = (0,0)):
-    plt.ion()   # matplotlib interactivate mode
+def brownMovement(N, t, n, starting=(0, 0)):
+    #plt.ion()   # matplotlib interactivate mode
 
     #plt.gca().set_aspect('equal', adjustable='box')
     x = starting[0]
     y = starting[1]
-    position = [[[x]
-                ,[y]]]
-    for i in range(n-1):
+    position = [[[x], [y]]]
+    for i in range(n - 1):
         position[0][0].append(x)
         position[0][1].append(y)
 
     #plt.rcParams['figure.figsize'] = (10.0, 10.0) # 设置figure_size尺寸
     #plt.rcParams['figure.dpi'] = 500 #分辨率
-    for i1 in range(int(N*t)-1):
-        position.append([[],[]])
+    for i1 in range(int(N * t) - 1):
+        position.append([[], []])
         for i2 in range(n):
-            position[i1+1][0].append(position[i1][0][i2]+1-2*random.random())
-            position[i1+1][1].append(position[i1][1][i2]+1-2*random.random())
-        x = plt.xlim(-20,20)
-        y = plt.ylim(-20,20)            
-        plt.scatter(position[i1][0],position[i1][1])
+            position[i1 + 1][0].append(position[i1][0][i2] + 1 -
+                                       2 * random.random())
+            position[i1 + 1][1].append(position[i1][1][i2] + 1 -
+                                       2 * random.random())
+        x = plt.xlim(-20, 20)
+        y = plt.ylim(-20, 20)
+        plt.scatter(position[i1][0], position[i1][1])
         plt.show()
         plt.pause(0.1)
         plt.clf()
 
-        #plt.scatter(position,position)
-    plt.show()
-
-def brownMovement3d(N,t,n,starting = (0,0,0)):
-    plt.ion()   # matplotlib interactivate mode
-    fig = plt.figure() 
-
-
-    x,y,z = starting[0],starting[1],starting[2]
-    position = [[[x],[y],[z]]]
-    disArray = []
-    disArrayMean = []
-
-    for i in range(n-1):
-        position[0][0].append(x)
-        position[0][1].append(y)
-        position[0][2].append(z)
-    for i1 in range(int(N*t)-1):
-        position.append([[],[],[]])
-        for i in range(n):
-            disArray.append([])
-        for i2 in range(n):
-            position[i1+1][0].append(position[i1][0][i2]+1-2*random.random())
-            position[i1+1][1].append(position[i1][1][i2]+1-2*random.random())
-            position[i1+1][2].append(position[i1][2][i2]+1-2*random.random())
-
-            x = position[i1][0][i2]
-            y = position[i1][1][i2]
-            z = position[i1][2][i2]
-
-            disArray[i2].append((x**2+y**2+z**2)**0.5)
-
-        ax = plt.subplot(121, projection='3d')
-
-
-        ax.set_xlim3d([-50, 50])
-        ax.set_ylim3d([-50, 50])
-        ax.set_zlim3d([-50, 50])
-
-        ax.scatter(position[i1][0],position[i1][1],position[i1][2])#,c="green")
-
-        disArrayMean.append(np.mean(np.array(disArray[i1]))) #绘制速度
-        vt= fig.add_subplot(122)
-        vt.plot(np.linspace(0,(i1+1)/N,i1+1),disArrayMean)
-
-        plt.show()
-        plt.pause(0.0001)
-        plt.clf()
-
-        #plt.scatter(position,position)
-    plt.show()
-
-brownMovement3d(1,1000,1000)
-
-
-
+brownMovement(1, 5, 1000)
---
+++ brownMovement3d.py
@@ -0,0 +1,89 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+from scipy import stats
+from mayavi import mlab
+
+def matDraw(position, i1):
+    ax = plt.subplot(121, projection='3d')
+    ax.set_xlim3d([-50, 50])
+    ax.set_ylim3d([-50, 50])
+    ax.set_zlim3d([-50, 50])
+    ax.scatter(position[i1][0], position[i1][1], position[i1][2])
+
+    plt.show()
+    plt.pause(0.0001)
+    plt.clf()
+
+
+def mayaviDraw(position):
+    coor = [position[-1][0], position[-1][1], position[-1][2]]
+
+    coor = np.stack(coor, axis=0)
+    kde = stats.gaussian_kde(coor)
+    density = kde(coor)
+
+    x = position[-1][0]
+    y = position[-1][1]
+    z = position[-1][2]
+
+    mlab.figure('DensityPlot')
+    mlab.points3d(x, y, z, density, scale_mode='none', scale_factor=0.07)
+    mlab.axes()
+    mlab.show()
+    mlab.clf()
+
+
+def brownMovement3d(N, t, n, dimension=(0, 0, 0)):
+    plt.ion()  # matplotlib interactivate mode
+    fig = plt.figure()
+
+    
+    x, y, z = dimension[0]/2, dimension[1]/2, dimension[2]/2
+    position = [[[x], [y], [z]]]
+    disArray = []
+    disArrayMean = []
+
+    for i in range(n - 1):  #生成初始点矩阵
+        position[0][0].append(x)
+        position[0][1].append(y)
+        position[0][2].append(z)
+
+    for i1 in range(int(N * t) - 1):  #随机行走，每个循环生成一帧中的情况
+        position.append([[], [], []])
+        for i in range(n):  #初始化
+            disArray.append([])
+        for i2 in range(n):  #每个循环计算出一个点在一帧中的变化
+            position[i1 + 1][0].append(position[i1][0][i2] + 1 -
+                                       2 * random.random())
+            position[i1 + 1][1].append(position[i1][1][i2] + 1 -
+                                       2 * random.random())
+            position[i1 + 1][2].append(position[i1][2][i2] + 1 -
+                                       2 * random.random())
+            #'''
+            x = position[i1][0][i2]
+            y = position[i1][1][i2]
+            z = position[i1][2][i2]
+
+            disArray[i1].append((x**2 + y**2 + z**2)**0.5)
+
+        ax = plt.subplot(121, projection='3d')
+        vt = fig.add_subplot(122)
+
+        disArrayMean.append(np.mean(np.array(disArray[i1])))  #计算平均速度
+        vt.plot(np.linspace(0, (i1 + 1) / N, i1 + 1), disArrayMean)
+            #'''
+        ax.set_xlim3d([-50, 50])
+        ax.set_ylim3d([-50, 50])
+        ax.set_zlim3d([-50, 50])
+        ax.scatter(position[i1][0], position[i1][1], position[i1][2])
+
+        plt.show()
+        plt.pause(1)
+        plt.clf()
+        #matDraw(position,i1)
+        #mayaviDraw(position)
+    plt.show()
+
+
+brownMovement3d(1000, 1000, 100000)
